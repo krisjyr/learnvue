2ba923d87b77777a37a4e7732afad39a390be58a
krisjyr <kristofer.jyrgenstein@gmail.com>
2023-10-12 11:33:19 +0300

    yeet

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref } from "vue";
+const message = ref("Hello Vue");
+</script>
+
+<template>
+<button v-on:click="message ='neeger' ">Click me</button>
+<h1>{{ message }}</h1>
+</template>
