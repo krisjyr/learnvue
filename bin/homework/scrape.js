49e3a7bfa4fecb7a5106d068051b8e47d9cdefe1
Krisjyr <kristofer.jyrgenstein@tptlive.ee>
2024-03-21 12:35:11 +0200

    added scraper

--- bin/homework/scrape.js
+++ bin/homework/scrape.js
@@ -1,26 +1,39 @@
-import axios from 'axios';
-import * as cheerio from 'cheerio';
-import fs from 'node:fs';
+import axios from "axios";
+import * as cheerio from "cheerio";
+import fs from "node:fs";
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
-if (!fs.existsSync('cache')) {
-    fs.mkdirSync('cache');
+if (!fs.existsSync("cache")) {
+  fs.mkdirSync("cache");
 }
 
+for (let i = 899; i <= 899; i++) {
+  let cacheName = `cache/${i}.html`;
+  let data;
+  if (!fs.existsSync(cacheName)) {
+    await sleep(1000);
+    let res = await axios.get(`https://gunshowcomic.com/${i}`);
+    data = res.data;
+    fs.writeFileSync(cacheName, data);
+  } else {
+    data = fs.readFileSync(cacheName);
+  }
+  const $ = cheerio.load(data);
+  console.log(`Comic nr ${i}`);
 
-for (let i = 16; i > 6; i--) {
+  let img = $(`#comic>img`);
 
-    let cacheName = `cache/${i}.html`;
-    let data;
-    if (!fs.existsSync(cacheName)) {
-        await sleep(1000);
-        let res = await axios.get(`https://www.doomworld.com/10years/doomcomic/comic.php?page=${i}/`);
-        data = res.data;
-        fs.writeFileSync(cacheName, data);
+  console.log(img.length);
+
+  for (let i = 0; i <= img.length; i++) {
+    console.log(i);
+    if (img.attr("src") !== undefined) {
+      console.log(img.attr("src"));
+      img = img.next();
     } else {
-        data = fs.readFileSync(cacheName);
+      img = img.next();
+      console.log(img.attr("src"));
+      img = img.next();
     }
-    const $ = cheerio.load(data);
-    let img = $(`td>table+center>a>img`);
-    console.log(img.attr('src'));
+  }
 }
